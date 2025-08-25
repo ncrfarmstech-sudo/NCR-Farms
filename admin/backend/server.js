c924ba4b1d57287661cbe40733f8f40981aa399d
affordindia <ncrfarmstech@gmail.com>
2025-08-25 12:30:17 +0530

    with backend connection with main website

--- admin/backend/server.js
+++ admin/backend/server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
       return res.sendStatus(200);
